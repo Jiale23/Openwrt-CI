2e8e1ae46fe31cf426adb074d76800c04c57e72d
Jiale23 <fjlfjl2015@hotmail.com>
2025-02-22 17:20:50 +0800

    Add cudy_tr3000

---
+++ Scripts/Handles.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+PKG_PATH="$GITHUB_WORKSPACE/wrt/package/"
+
+#修改argon主题字体和颜色
+if [ -d *"luci-theme-argon"* ]; then
+	cd ./luci-theme-argon/
+
+	sed -i '/font-weight:/ {/!important/! s/\(font-weight:\s*\)[^;]*;/\1normal;/}' $(find ./luci-theme-argon -type f -iname "*.css")
+	sed -i "s/primary '.*'/primary '#31a1a1'/; s/'0.2'/'0.5'/; s/'none'/'bing'/" ./luci-app-argon-config/root/etc/config/argon
+
+	cd $PKG_PATH && echo "theme-argon has been fixed!"
+fi
+
+#修复Coremark编译失败
+CM_FILE=$(find ../feeds/packages/ -maxdepth 3 -type f -wholename "*/coremark/Makefile")
+if [ -f "$CM_FILE" ]; then
+	sed -i 's/mkdir/mkdir -p/g' $CM_FILE
+
+	cd $PKG_PATH && echo "coremark has been fixed!"
+fi
